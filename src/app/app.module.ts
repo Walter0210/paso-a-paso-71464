0b38399edbaa9c0041387cfb5ecaf5c8ce222929
Walter0210 <walter.ebalderrama@gmail.com>
2021-06-23 00:27:16 -0300

    2 files modified, 5 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ListaArticulosComponent } from './lista-articulos/lista-articulos.component';
+import { MenuComponent } from './menu/menu.component';
+import { ArticulosComponent } from './articulos/articulos.component';
 
 
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, ListaArticulosComponent, ],
+  declarations: [ AppComponent, HelloComponent, ListaArticulosComponent, MenuComponent, ArticulosComponent, ],
   bootstrap:    [ AppComponent ],
 })
 export class AppModule { }
